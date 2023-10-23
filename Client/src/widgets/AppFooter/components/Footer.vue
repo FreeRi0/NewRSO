7c07f489b8ab67d7563b35de73ff6210484bb17d
kozyrek <kozyrevaia@bk.ru>
2023-10-23 11:54:56 +0500

    добавляет header и footer

--- Client/src/widgets/AppFooter/components/Footer.vue
+++ Client/src/widgets/AppFooter/components/Footer.vue
@@ -1,133 +1,80 @@
 <template>
   <div class="footer">
-    <div class="container">
-                <!-- Вехний контейнер футера -->
-                <div class="footer__top-container">
-                    <!-- Первая колонка  -->
-                    <!-- Логотип РСО -->
-                    <div class="footer__logo-wrapper">
-                        <div class="footer__logo-rso">
-                            <img src="../../../app/assets/logo/rso-white.svg" width="80" height="72" alt="Логотип сайта РСО">
-                        </div>
-                        <!-- Иконки соцсетей -->
-                        <ul class="footer__social-list">
-                          <li class="footer__social-item">
-                            <a href="https://vk.com/rso_official" target="_blank">
-                              <img src="../../../app/assets/logo/vk-white.svg" width="28" height="28" alt="Логотип группы Вконтакте">
-                            </a>
-                          </li>
-                          <li class="footer__social-item">
-                            <a href="https://t.me/rso_official" target="_blank">
-                              <img src="../../../app/assets/logo/telegram-white.svg" width="28" height="28" alt="Логотип канала в Телеграмм">
-                            </a>
-                          </li>
-                        </ul>
-                    </div>
-                    <!-- Вторая колонка Адрес-->
-                    <!-- <div class="footer__contacts-address"> -->
-                        <p class="footer__contacts-address">105005, г. Москва, Лефортовский переулок, 8, строение 1</p>
-                    <!-- </div> -->
-                    <!-- Третья колонка Контакты-->
-                    <ul class="footer__contacts">
-                        <li class="footer__contacts-telephone">
-                            <a href="tel:+74992613345">8 499 261 33 45</a>
-                        </li>
-                        <li class="footer__contacts-telephone">
-                            <p class="footer__text-p">Горячая линия РСО:</p>
-                            <a href="tel:+78007700117">8 800 770 0 117</a>
-                        </li>
-                        <li class="footer__contacts-email">
-                            <a href="mailto:r.s.o@mail.ru">r.s.o@mail.ru</a>
-                        </li>
-                    </ul>
-                    <!-- Четвёртая колонка Политика конфиденциальности Пользовательское соглашение -->
-                    <ul class="footer__privacy-policy-list">
-                      <li class="footer__privacy-policy-item">
-                          <a href="#">Политика конфиденциальности</a>
-                      </li>
-                      <li class="footer__user-agreement">
-                          <a href="#">Пользовательское соглашение</a>
-                      </li>
-                    </ul>
-                </div>
-                <!-- Нижний контейнер футера -->
-                <div class="footer__bottom-container">
-                    <!-- Левая часть футера -->
-                    <div class="footer__all-rights-reserved">
-                        <p>&copy; 1959-2023 РОССИЙСКИЕ СТУДЕНЧЕСКИЕ ОТРЯДЫ.</p>
-                        <p>Все права защищены.</p>
-                    </div>
-                    <!-- Правая часть футера-->
-                    <div class="footer__developed">
-                      <p>Сделано в</p>
-                      <!-- Логотип 1Т | Teamcode -->
-                      <!-- <div class="footer__logo-teamcode"> -->
-                        <a class="footer__logo-teamcode-link" href="https://club.1t.ru/work" target="_blank">
-                            <img src="../../../app/assets/logo/logo-1t-club-white.svg" width="80" height="24" alt="Логотип команда 1Т Клуб">
-                            <span>Teamcode</span>
-                        </a>
-                      <!-- </div> -->
-                    </div>
-                </div>
-            </div>
+    <div class="container">                
+      <div class="footer__top-container">
+        <div class="footer__logo-wrapper">
+          <div class="footer__logo-rso">
+            <img src="../../../app/assets/logo/rso-white.svg" width="80" height="72" alt="Логотип сайта РСО">
+          </div>
+
+          <ul class="footer__social-list">
+            <li class="footer__social-item">
+              <a href="https://vk.com/rso_official" target="_blank">
+                <img src="../../../app/assets/logo/vk-white.svg" width="28" height="28" alt="Логотип группы Вконтакте">
+              </a>
+            </li>
+
+            <li class="footer__social-item">
+              <a href="https://t.me/rso_official" target="_blank">
+                <img src="../../../app/assets/logo/telegram-white.svg" width="28" height="28" alt="Логотип канала в Телеграмм">
+              </a>
+            </li>
+          </ul>
+        </div>
+
+        <p class="footer__contacts-address">105005, г. Москва, Лефортовский переулок, 8, строение 1</p>
+
+        <ul class="footer__contacts">
+          <li class="footer__contacts-telephone">
+            <a href="tel:+74992613345">8 499 261 33 45</a>
+          </li>
+          <li class="footer__contacts-telephone">
+            <p class="footer__text-p">Горячая линия РСО:</p>
+            <a href="tel:+78007700117">8 800 770 0 117</a>
+          </li>
+          <li class="footer__contacts-email">
+            <a href="mailto:r.s.o@mail.ru">r.s.o@mail.ru</a>
+          </li>
+        </ul>
+
+        <ul class="footer__privacy-policy-list">
+          <li class="footer__privacy-policy-item">
+            <a href="#">Политика конфиденциальности</a>
+          </li>
+          <li class="footer__user-agreement">
+            <a href="#">Пользовательское соглашение</a>
+          </li>
+        </ul>
+      </div>
+
+      <div class="footer__bottom-container">
+        <div class="footer__all-rights-reserved">
+          <p>&copy; 1959-2023 РОССИЙСКИЕ СТУДЕНЧЕСКИЕ ОТРЯДЫ.</p>
+          <p>Все права защищены.</p>
+        </div>
+
+        <div class="footer__developed">
+          <p>Сделано в</p>
+          <a class="footer__logo-teamcode-link" href="https://club.1t.ru/work" target="_blank">
+              <img src="../../../app/assets/logo/logo-1t-club-white.svg" width="80" height="24" alt="Логотип команда 1Т Клуб">
+              <span>Teamcode</span>
+          </a>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
+
 <script>
+
 export default {
 
 }
 </script>
+
 <style scoped lang="scss">
-
-//Общие стили
-
-body div[id="app"] {
-    display: flex;
-    flex-direction: column;
-    min-height: 100vh;
-}
-
-ul {
-list-style: none;
-}
-
-a {
-text-decoration: none;
-}
-  
-.container {
-margin: 0 auto;
-padding: 0 130px;
-max-width: 1440px;
-width: 100%;
-// background-color: #ffffff;/*переменная??*/
-
-@media(max-width:1440px) {
-    padding: 0 66px;
-    max-width: 1312px;
-}
-
-@media(max-width: 1024px) {
-    padding: 0 34px;
-    max-width: 960px;
-}
-
-@media(max-width: 768px) {
-    padding: 0 16px;
-    max-width: 732px;
-}
-
-@media(max-width: 360px) {
-    padding: 0 16px;
-}
-}
-  
-//Стили компонента
 .footer {
-    margin-top: auto;
-}
-
-.footer {
+  margin-top: auto;
   padding: 48px 0 32px;
   color: #ffffff;
   background-color: #1c5c94;
--- Client/src/widgets/AppHeader/components/Header.vue
+++ Client/src/widgets/AppHeader/components/Header.vue
@@ -1,13 +1,208 @@
 <template>
-  <div>
-    Header Vue
+  <div class="container">
+    <header class="header">
+
+      <ul class="header__logo">
+        <li class="header__logo-rso">
+          <a href="https://трудкрут.рф/" target="_blank">
+            <img src="../../../app/assets/logo/logo-rso-tablet.svg" width="60" height="58" alt="Логотип сайта трудкрут.рф">
+          </a>
+        </li>
+
+        <li class="header__logo-labor-cool">
+          <a href="https://trudkrutshop.ru/" target="_blank">
+            <img src="../../../app/assets/logo/logo-shop-tablet.svg" width="56" height="56" alt="Логотип сайта trudkrutshop.ru">
+          </a>
+        </li>
+      </ul>
+
+      <nav class="header__nav nav-menu">        
+        <button class="header__button-mobile-menu" type="button"></button>
+        <ul class="header__nav-list no-visible">
+            <li class="header__nav-item">
+              <div class="dropdown nav-menu">
+                <Dropdown/>
+              </div>
+            </li>
+            <li class="header__nav-item">
+              <a class="header__nav-link" href="#">Мероприятия</a>
+            </li>
+            <li class="header__nav-item">
+              <a class="header__nav-link" href="#">Полезная информация</a>
+            </li>
+        </ul>
+      </nav>
+
+      <nav class="header__nav nav-user">
+        <div class="application-count">
+          <a href=""><!--КОЛОКОЛЬЧИК ЭТО ССЫЛКА?? ЧТО ПРОИСХОДИТ ПРИ НАЖАТИИ??-->
+            <img src="../../../app/assets/icon/bell-light.svg" width="36" height="36" alt="Иконка уведомления">
+          </a>
+          <div class="quantity"></div>
+        </div>
+
+        <button class="nav-user__location">
+          <!--прописать в span кнопки логику изменения ее названия-->
+          <span>Карачаево-Черкесское региональное отделение</span>
+        </button>
+
+        <div class="dropdown user-menu">
+          <button type="button">
+            <img src="../../../app/assets/avatar-user.svg" alt="Фотография пользователя" width="56" height="56">
+          </button>
+          <ul>
+            <li class=""><a href="#">Моя страница</a></li>
+            <li class=""><a href="#">Мой отряд</a></li>
+            <li class=""><a href="#">Штаб СО ОО</a></li>
+            <li class=""><a href="#">Местный штаб</a></li>
+            <li class=""><a href="#">Региональный штаб</a></li>
+            <li class=""><a href="#">Окружной штаб</a></li>
+            <li class=""><a href="#">Активные заявки</a></li>
+            <li class=""><a href="#">Поиск участников</a></li>
+            <li class=""><a href="#">Членский взнос</a></li>
+            <li class=""><a href="#">Оформление справок</a></li>
+            <li class=""><a href="#">Настройки профиля</a></li>
+            <li class=""><a href="#">Выйти из ЛК</a></li>
+          </ul>
+        </div>
+      </nav> 
+    </header>
   </div>
 </template>
-<script>
-export default {
 
+<script>
+
+import { Dropdown } from '@shared/components/dropdown'
+
+export default {
+  components: {
+    Dropdown,
+  },
 }
 </script>
-<style lang="scss">
 
+<style scoped lang="scss">
+
+.header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 15px 0;
+  color: #35383f;
+  position: relative;
+
+  a {
+    color: #35383f;
+  }
+}
+
+.header__logo {
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  column-gap: 30px;
+  max-width: 146px;
+}
+
+.nav-menu {
+  @media(max-width: 1024px) {
+    order: 3;
+  }
+}
+
+.header__nav-list {
+  display: grid;
+  grid-template-columns: auto auto auto;
+  column-gap: 32px;
+  max-width: 445px;
+
+  @media(max-width: 1024px) {
+    position: absolute;
+    right: 0;
+    top: calc(100%);
+    padding: 28px;
+    border-radius: 10px;
+    grid-template-columns: 1fr;
+    row-gap: 8px;
+    background-color: #1f7cc0;
+
+  a {
+    color: #ffffff;
+  }
+  }
+}
+
+.header__nav-list.no-visible {
+  @media(max-width: 1024px) {
+    display: none;
+  }
+}
+
+.header__button-mobile-menu {
+  display: none;
+  position: relative;
+
+  @media(max-width: 1024px) {
+    display: block;
+    width: 36px;
+    height: 36px;
+    margin-left: 60px;
+    padding: 6px 4px;
+
+    &::before {
+      position: absolute;
+      content: "";
+      width: 24px;
+      height: 1px;
+      top: 8px;
+      left: 6px;
+      right: 6px;
+      background-color: #35383f;
+      box-shadow: 0 10px 0 0#35383f,0 20px 0 0#35383f;
+    }
+  }
+}
+
+.header__nav-item {
+  @media(max-width: 1024px) {
+    &:not(:last-child) {
+      border-bottom: 1px solid rgba(255, 255, 255, 0.2);
+    }
+  }
+}
+
+.header__nav-link {
+  display: block;
+  padding: 11px 0;
+}
+
+.nav-user {
+  display: grid;
+  grid-template-columns: 36px min-content 82px;
+  column-gap: 48px;
+  align-items: center;
+
+  &__location {
+    min-width: 190px;//скорректировать после шрифтов
+  }
+
+  @media(max-width: 1024px) {
+    margin-left: auto;
+    column-gap: 60px;
+  }
+}
+
+// .nav-menu {
+//   display: none;
+// }
+
+.user-menu ul {
+  display: none;
+}
+
+.application-count a {
+  display: block;
+  width: 36px;
+  height: 36px;
+  background-image: url('../../../app/assets/icon/bell-light.svg');
+}
 </style>
