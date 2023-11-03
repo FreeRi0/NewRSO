fa95b7a5660150377e3897f5fe4fdc013b2b85c8
kozyrek <kozyrevaia@bk.ru>
2023-11-03 15:50:33 +0500

    добавляет поля с выбором направления, института, добавление фото

--- Client/src/shared/components/selects/index.ts
+++ Client/src/shared/components/selects/index.ts
@@ -1,7 +1,6 @@
-
-import SelectRegion from "./SelectRegion.vue";
-import sortByEducation from "./sortByEdutaion.vue";
+import SelectRegion from './SelectRegion.vue';
+import sortByEducation from './sortByEdutaion.vue';
 import Select from './Select.vue';
+import SearchSelect from './SearchSelect.vue';
 
-export {SelectRegion, sortByEducation, Select};
-
+export { SelectRegion, sortByEducation, Select, SearchSelect };
