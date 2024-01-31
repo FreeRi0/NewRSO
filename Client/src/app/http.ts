c528b9b7783019523da3896782beede9096f47ac
Sergio Alferov <alferovsergio@yandex.ru>
2024-01-31 20:54:33 +0300

    change urll

--- Client/src/app/http.ts
+++ Client/src/app/http.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 export const HTTP = axios.create({
     // baseURL: process.env.VUE_APP_URL,//
 
-    // baseURL: 'https://xn--j1ab.xn--d1amqcgedd.xn--p1ai/api/v1/' // -> MAIN-ветка
-    baseURL: 'https://rso.sprint.1t.ru/api/v1' // -> DEV-ветка
+    baseURL: 'https://xn--j1ab.xn--d1amqcgedd.xn--p1ai/api/v1/' // -> MAIN-ветка
+    // baseURL: 'https://rso.sprint.1t.ru/api/v1' // -> DEV-ветка
     // baseURL: 'http://127.0.0.1:8000/api/v1'
 });
