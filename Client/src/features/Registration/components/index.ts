60ec31ed987e30a5b817417b226714d6089cab6d
Sergio Alferov <alferovsergio@yandex.ru>
2024-02-07 20:25:26 +0300

    fix build

--- Client/src/features/Registration/components/index.ts
+++ Client/src/features/Registration/components/index.ts
@@ -1,9 +1,3 @@
 import RegistrationForm from './Registration.vue';
-import { createApp } from 'vue';
-import { VueMaskDirective } from 'v-mask';
-
-const app = createApp(RegistrationForm);
-app.config.productionTip = false;
-app.directive('mask', VueMaskDirective);
 
 export { RegistrationForm };
