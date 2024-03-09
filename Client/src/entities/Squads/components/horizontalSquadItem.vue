260c0062e7254ec3a3164a05944f99c34987573c
Sergio Alferov <alferovsergio@yandex.ru>
2024-03-09 20:40:09 +0300

    fix

--- Client/src/entities/Squads/components/horizontalSquadItem.vue
+++ Client/src/entities/Squads/components/horizontalSquadItem.vue
@@ -16,9 +16,6 @@
             </div>
 
         </router-link>
-        <div class="horizontal-item ml-3" v-if="member?.detachment !== null">
-                <p class="horizontal-item-title ml-2">{{ member.junior_detachment?.regional_headquarter_name }}</p>
-            </div>
         <router-link :to="{ name: 'lso', params: { id: member.junior_detachment.id } }"
             class="horizontal-item ml-3"
         >
