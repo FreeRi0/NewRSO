816c8dd7f6f146dc865511aab005a48bb1021c28
Sergio Alferov <alferovsergio@yandex.ru>
2024-04-01 10:36:32 +0300

    add button

--- Client/src/features/Competition/components/CompetitionPromo.vue
+++ Client/src/features/Competition/components/CompetitionPromo.vue
@@ -83,7 +83,7 @@
             </div>
         </div>
 
-        <div>
+        <div v-if="roleStore.status.is_commander_detachment">
             <router-link
                 :to="{
                     name: 'debut',
@@ -98,6 +98,7 @@
 
 <script setup>
 import { ref, onMounted, onUnmounted } from 'vue';
+import { useRoleStore } from '@layouts/store/role';
 
 defineProps({
     squad: {
@@ -107,7 +108,7 @@ defineProps({
 });
 
 let sizeImage = ref('desktop');
-
+const roleStore = useRoleStore();
 const getSizeImage = () => {
     console.log('ширина экрана', window.innerWidth);
     if (window.innerWidth <= 360) {
