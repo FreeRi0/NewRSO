7ced7e1f134da35d276726e0203446b6ead39ffa
boyoomi <135057713+boyoomi@users.noreply.github.com>
2024-04-05 08:28:30 +0300

    +

--- Client/src/pages/ReportPage/components/ReportPage.vue
+++ Client/src/pages/ReportPage/components/ReportPage.vue
@@ -2363,7 +2363,7 @@ s
                     <v-expansion-panel-text class="form__inner-content">
                         <div class="form__field-group">
                             <div
-                                class="form__field-group-top form__field-column-one"
+                                class="form__field-group-top form__field-column-one form__field-column-one-file"
                                 v-for="(block, index) in report[13]
                                     .organization_data"
                                 :key="index"
@@ -2398,6 +2398,32 @@ s
                                             {{ counterReport }} / 100
                                         </div>
                                     </div>
+                                </div>
+                                <div type="button">
+                                    <svg
+                                        @click="deleteBlock13(index)"
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        width="24"
+                                        height="24"
+                                        viewBox="0 0 24 24"
+                                        fill="none"
+                                    >
+                                        <path
+                                            d="M12 21C10.8181 21 9.64778 20.7672 8.55585 20.3149C7.46392 19.8626 6.47177 19.1997 5.63604 18.364C4.80031 17.5282 4.13738 16.5361 3.68508 15.4442C3.23279 14.3522 3 13.1819 3 12C3 10.8181 3.23279 9.64778 3.68508 8.55585C4.13738 7.46392 4.80031 6.47177 5.63604 5.63604C6.47177 4.80031 7.46392 4.13738 8.55585 3.68508C9.64778 3.23279 10.8181 3 12 3C13.1819 3 14.3522 3.23279 15.4442 3.68508C16.5361 4.13738 17.5282 4.80031 18.364 5.63604C19.1997 6.47177 19.8626 7.46392 20.3149 8.55585C20.7672 9.64778 21 10.8181 21 12C21 13.1819 20.7672 14.3522 20.3149 15.4442C19.8626 16.5361 19.1997 17.5282 18.364 18.364C17.5282 19.1997 16.5361 19.8626 15.4441 20.3149C14.3522 20.7672 13.1819 21 12 21L12 21Z"
+                                            stroke="#939393"
+                                            stroke-linecap="round"
+                                        />
+                                        <path
+                                            d="M9 9L15 15"
+                                            stroke="#939393"
+                                            stroke-linecap="round"
+                                        />
+                                        <path
+                                            d="M15 9L9 15"
+                                            stroke="#939393"
+                                            stroke-linecap="round"
+                                        />
+                                    </svg>
                                 </div>
                             </div>
                             <div
@@ -2490,7 +2516,7 @@ s
                     <v-expansion-panel-text class="form__inner-content">
                         <div class="form__field-group">
                             <div
-                                class="form__field-group-top form__field-column-one"
+                                class="form__field-group-top form__field-column-one form__field-column-one-file"
                                 v-for="(block, index) in report[14]
                                     .q14_labor_project"
                                 :key="index"
@@ -2530,6 +2556,32 @@ s
                                             {{ counterReport }} / 100
                                         </div>
                                     </div>
+                                </div>
+                                <div type="button">
+                                    <svg
+                                        @click="deleteBlock14(index)"
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        width="24"
+                                        height="24"
+                                        viewBox="0 0 24 24"
+                                        fill="none"
+                                    >
+                                        <path
+                                            d="M12 21C10.8181 21 9.64778 20.7672 8.55585 20.3149C7.46392 19.8626 6.47177 19.1997 5.63604 18.364C4.80031 17.5282 4.13738 16.5361 3.68508 15.4442C3.23279 14.3522 3 13.1819 3 12C3 10.8181 3.23279 9.64778 3.68508 8.55585C4.13738 7.46392 4.80031 6.47177 5.63604 5.63604C6.47177 4.80031 7.46392 4.13738 8.55585 3.68508C9.64778 3.23279 10.8181 3 12 3C13.1819 3 14.3522 3.23279 15.4442 3.68508C16.5361 4.13738 17.5282 4.80031 18.364 5.63604C19.1997 6.47177 19.8626 7.46392 20.3149 8.55585C20.7672 9.64778 21 10.8181 21 12C21 13.1819 20.7672 14.3522 20.3149 15.4442C19.8626 16.5361 19.1997 17.5282 18.364 18.364C17.5282 19.1997 16.5361 19.8626 15.4441 20.3149C14.3522 20.7672 13.1819 21 12 21L12 21Z"
+                                            stroke="#939393"
+                                            stroke-linecap="round"
+                                        />
+                                        <path
+                                            d="M9 9L15 15"
+                                            stroke="#939393"
+                                            stroke-linecap="round"
+                                        />
+                                        <path
+                                            d="M15 9L9 15"
+                                            stroke="#939393"
+                                            stroke-linecap="round"
+                                        />
+                                    </svg>
                                 </div>
                             </div>
                             <div
@@ -2623,7 +2675,7 @@ s
                     <v-expansion-panel-text class="form__inner-content">
                         <div class="form__field-group">
                             <div
-                                class="form__field-group-top form__field-column-one"
+                                class="form__field-group-top form__field-column-one form__field-column-one-file"
                                 v-for="(block, index) in report[15].grants_data"
                                 :key="index"
                             >
@@ -2704,6 +2756,32 @@ s
                                             {{ counterReport }} / 100
                                         </div>
                                     </div>
+                                </div>
+                                <div type="button">
+                                    <svg
+                                        @click="deleteBlock15(index)"
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        width="24"
+                                        height="24"
+                                        viewBox="0 0 24 24"
+                                        fill="none"
+                                    >
+                                        <path
+                                            d="M12 21C10.8181 21 9.64778 20.7672 8.55585 20.3149C7.46392 19.8626 6.47177 19.1997 5.63604 18.364C4.80031 17.5282 4.13738 16.5361 3.68508 15.4442C3.23279 14.3522 3 13.1819 3 12C3 10.8181 3.23279 9.64778 3.68508 8.55585C4.13738 7.46392 4.80031 6.47177 5.63604 5.63604C6.47177 4.80031 7.46392 4.13738 8.55585 3.68508C9.64778 3.23279 10.8181 3 12 3C13.1819 3 14.3522 3.23279 15.4442 3.68508C16.5361 4.13738 17.5282 4.80031 18.364 5.63604C19.1997 6.47177 19.8626 7.46392 20.3149 8.55585C20.7672 9.64778 21 10.8181 21 12C21 13.1819 20.7672 14.3522 20.3149 15.4442C19.8626 16.5361 19.1997 17.5282 18.364 18.364C17.5282 19.1997 16.5361 19.8626 15.4441 20.3149C14.3522 20.7672 13.1819 21 12 21L12 21Z"
+                                            stroke="#939393"
+                                            stroke-linecap="round"
+                                        />
+                                        <path
+                                            d="M9 9L15 15"
+                                            stroke="#939393"
+                                            stroke-linecap="round"
+                                        />
+                                        <path
+                                            d="M15 9L9 15"
+                                            stroke="#939393"
+                                            stroke-linecap="round"
+                                        />
+                                    </svg>
                                 </div>
                             </div>
                             <div
@@ -3655,6 +3733,18 @@ const deleteBlock11 = (index) => {
 
 const deleteBlock12 = (index) => {
     report.value[12].participation_data.splice(index, 1);
+};
+
+const deleteBlock13 = (index) => {
+    report.value[13].organization_data.splice(index, 1);
+};
+
+const deleteBlock14 = (index) => {
+    report.value[14].q14_labor_project.splice(index, 1);
+};
+
+const deleteBlock15 = (index) => {
+    report.value[15].grants_data.splice(index, 1);
 };
 
 const report = ref({
