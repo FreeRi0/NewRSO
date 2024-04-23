5b6439e6d7004a95594c8a61e9f6c16f8b351ec0
Sergio Alferov <alferovsergio@yandex.ru>
2024-04-23 08:50:46 +0300

    hide

--- Client/src/pages/SafetyPage/components/SafetyPage.vue
+++ Client/src/pages/SafetyPage/components/SafetyPage.vue
@@ -203,9 +203,7 @@
             </iframe>
         </div>
 
-        <div id="testing" class="corpuniver__documents-test"
-            v-if="roleStore.status.is_commander_detachment ||
-                roleStore.status.is_commissar_detachment || (squadsStore.competitionSquads.find((item) => item.detachment?.id == userStore.currentUser.detachment_id || item.junior_detachment?.id == userStore.currentUser.detachment_id))">
+        <div id="testing" class="corpuniver__documents-test">
             <p class="text corpuniver__documents-description-test">
                 Итоговую аттестацию можно пройти с 22 апреля по 15 мая 2024 года (допускается 1 пересдача)
                 Для прохождения тестирования необходимо набрать не менее 60 баллов. Удачи!
