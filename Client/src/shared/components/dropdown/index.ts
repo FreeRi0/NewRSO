8920ff7a1b69b0e488ff18e71c9db27f482c8e3c
Sergio Alferov <alferovsergio@yandex.ru>
2024-05-06 19:01:36 +0300

    fix

--- Client/src/shared/components/dropdown/index.ts
+++ Client/src/shared/components/dropdown/index.ts
@@ -1,5 +1,5 @@
 import Dropdown from "./Dropdown.vue";
 import CustomDropdown from './CustomDropdown.vue';
-import DeleteModal from "./DeleteModal.vue";
+import DeleteModal from "./deleteModal.vue";
 
 export { Dropdown, CustomDropdown, DeleteModal };
