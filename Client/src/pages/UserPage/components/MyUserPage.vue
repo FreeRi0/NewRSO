2155eb8e9dec4127c13006995b522a9a3d46dd55
Sergio Alferov <alferovsergio@yandex.ru>
2024-05-31 13:50:49 +0300

    token

--- Client/src/pages/UserPage/components/MyUserPage.vue
+++ Client/src/pages/UserPage/components/MyUserPage.vue
@@ -61,10 +61,11 @@ const region = ref({});
 const query = new URLSearchParams(window.location.search);
 const payload  = JSON.parse(query.get("payload"));
 
+console.log(payload, payload.silent_token, payload.token)
 
 
 const TokenData = ref({
-    silent_token: payload?.silent_token,
+    silent_token: payload?.token,
     uuid: payload?.uuid,
 })
 
