9ec08dbbab19253e40ddae57c73cae412347cba8
Sergio Alferov <alferovsergio@yandex.ru>
2024-05-31 21:07:18 +0300

    hide vk

--- Client/src/pages/UserPage/components/MyUserPage.vue
+++ Client/src/pages/UserPage/components/MyUserPage.vue
@@ -58,30 +58,28 @@ const education = ref({});
 const region = ref({});
 
 
-const query = new URLSearchParams(window.location.search);
-const payload = JSON.parse(query.get("payload"));
-
-// console.log(payload, payload?.silent_token, payload?.token)
+// const query = new URLSearchParams(window.location.search);
+// const payload = JSON.parse(query.get("payload"));
 
 
-const TokenData = ref({
-    silent_token: payload?.token,
-    uuid: payload?.uuid,
-    access_token: import.meta.env.VITE_API_SERVICE_TOKEN
-})
+
+// const TokenData = ref({
+//     silent_token: payload?.token,
+//     uuid: payload?.uuid,
+// })
 
 
-const exchangeToken = async () => {
-    try {
-        const resp = await HTTP.post('/exchange-token/', TokenData.value, {
-            headers: {
-                'Content-Type': 'application/json',
-            },
-        })
-    } catch (e) {
-        console.log('error:', e)
-    }
-}
+// const exchangeToken = async () => {
+//     try {
+//         const resp = await HTTP.post('/exchange-token/', TokenData.value, {
+//             headers: {
+//                 'Content-Type': 'application/json',
+//             },
+//         })
+//     } catch (e) {
+//         console.log('error:', e)
+//     }
+// }
 
 const uploadAva = (imageAva) => {
 
@@ -113,9 +111,9 @@ const deleteWall = (imageWall) => {
     currentUser.currentUser.value.media.banner = imageWall;
 };
 
-onMounted(() => {
-    exchangeToken();
-})
+// onMounted(() => {
+//     exchangeToken();
+// })
 </script>
 <style lang="scss" scoped>
 .user-wrapper {
