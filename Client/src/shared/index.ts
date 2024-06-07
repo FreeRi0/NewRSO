d123c5dabe3c1f762e33c8f4a9294c71affbe8dc
Styxk1 <styxk96@gmail.com>
2024-06-07 12:05:45 +0300

    Экспорт компонента для отрисовки svg иконок

--- Client/src/shared/index.ts
+++ Client/src/shared/index.ts
@@ -13,3 +13,5 @@ export { usePageStore } from './stores/pageStore';
 export { default as usePage } from './composables/usePage';
 export { default as useCrosspageFilter } from './composables/useCrosspageFilter';
 /* exports composables */
+
+export { SvgIcon } from './components/SvgIcon';
