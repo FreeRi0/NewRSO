ae4d344cf00a3e4f10ee6ba00cece4b70cb9a477
Sergio Alferov <alferovsergio@yandex.ru>
2024-06-07 17:18:53 +0300

    fix

--- Client/src/app/http.ts
+++ Client/src/app/http.ts
@@ -11,7 +11,7 @@ export const HTTP = axios.create({
 HTTP.interceptors.request.use(
     (config) => {
         // console.log(config.headers.Accept);
-        if (config.url == '/token/login/' || config.url == '/register/') {
+        if (config.url == '/token/login/' || config.url == '/register/' || config.url == '/exchange-token/') {
             delete config.headers.Authorization;
         } else if(config.url == 'rsousers/me/') {
             config.headers.Authorization = 'Bearer ' + localStorage.getItem('Bearer');
