2aef8f08ef8df63816fdb16cc02f9ac8b81f6b82
Sergio Alferov <alferovsergio@yandex.ru>
2024-06-07 16:43:01 +0300

    fix

--- Client/src/layouts/MainLayout/components/MainLayout.vue
+++ Client/src/layouts/MainLayout/components/MainLayout.vue
@@ -58,13 +58,14 @@ import { useSquadsStore } from '@features/store/squads';
 import { useDistrictsStore } from '@features/store/districts';
 
 const roleStore = useRoleStore();
+// const userStore = useUserStore();
 const regionsStore = useRegionalsStore();
 const positionsStore = usePositionsStore();
 const competition_pk = 1;
 // const districtStore  = useDistrictsStore();
 
 const squadsStore = useSquadsStore();
-const currentUser = storeToRefs(userStore);
+// const currentUser = storeToRefs(userStore);
 const isAuth = ref(!!localStorage.getItem('Token'));
 
 onMounted(() => {
