9e5e7fe0a79222397140398a262e269b45e12e41
Sergio Alferov <alferovsergio@yandex.ru>
2024-06-11 16:24:20 +0300

    fix

--- Client/src/app/http.ts
+++ Client/src/app/http.ts
@@ -1,5 +1,4 @@
 import axios from 'axios';
-import routes from './router/routes';
 export const HTTP = axios.create({
     // baseURL: 'https://xn--j1ab.xn--d1amqcgedd.xn--p1ai/api/v1/'
     baseURL: 'https://rso.sprint.1t.ru/api/v1/',
