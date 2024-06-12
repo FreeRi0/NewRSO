97f487089816d16cbb96baa447431f90b1ecba8f
boyoomi <135057713+boyoomi@users.noreply.github.com>
2024-06-12 20:04:43 +0300

    +

--- Client/src/mixins/mixins.ts
+++ Client/src/mixins/mixins.ts
@@ -5,7 +5,7 @@ const mixins = {
         };
     },
     methods: {
-        getEnding(count) {
+        getEnding(count: number) {
             if (count === 1 && count % 100 !== 11) {
                 return 'участник';
             } else if ([2, 3, 4].includes(count)) {
@@ -14,7 +14,7 @@ const mixins = {
                 return 'участников';
             }
         },
-        getEndingMembers(count) {
+        getEndingMembers(count: number) {
             if (count === 1 && count % 100 !== 11) {
                 return 'действующий член';
             } else if ([2, 3, 4].includes(count)) {
