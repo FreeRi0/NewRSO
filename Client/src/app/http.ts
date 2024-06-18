a8f9d369833550434a4c7afe7a29a6c57d129401
Sergio Alferov <alferovsergio@yandex.ru>
2024-06-18 19:23:35 +0300

    fix

--- Client/src/app/http.ts
+++ Client/src/app/http.ts
@@ -12,7 +12,7 @@ export const HTTP = axios.create({
 HTTP.interceptors.request.use(
     (config) => {
 
-        if (config.url == '/jwt/create/' || config.url == '/register/' || config.url == '/exchange-token/' || config.url == '/jwt/vk-login/' || config.url == '/jwt/verify/' || config.url == '/jwt/refresh/' || config.url == '/regions/' || config.url == '/reset_password/') {
+        if (config.url == '/jwt/create/' || config.url == '/register/' || config.url == '/exchange-token/' || config.url == '/jwt/vk-login/' || config.url == '/jwt/verify/' || config.url == '/jwt/refresh/' || config.url == '/regions/' || config.url == '/reset_password/' || config.url == '/users/reset_password_confirm/') {
             delete config.headers.Authorization;
         } else {
             config.headers.Authorization =
