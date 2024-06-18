44106fca2ba1ff8cc9767feeef61aa3a5653a8c5
Styxk1 <styxk96@gmail.com>
2024-06-18 17:56:14 +0300

    Компонент активных заявок в штаб

---
+++ Client/src/features/ActiveApplicationsHeadquarters/components/ActiveHeadquarters.vue
@@ -0,0 +1,240 @@
+<template>
+    <div class="competitions__container">
+        <p v-if="loading">Загрузка...</p>
+
+        <template v-else>
+            <div class="competitions__actions">
+                <div class="horizontal__confidant">
+                    <input type="checkbox" v-model="isCheckedAll" @change="onChooseAll" />
+                </div>
+                <v-select variant="outlined" class="form__field competitions__actions-select" :items="actionsList"
+                    v-model="action" placeholder="Выберите действие" />
+            </div>
+
+            <div class="competitions__list">
+                <template v-for="(application, index) in applicationsData" :key="application.id">
+                    <active-headquarters-item :application="application" :position="index" @select="onToggleSelect" />
+                </template>
+
+                <template v-if="selectedApplications.length">
+                    <p class="text_total">
+                        Итого: {{ selectedApplications.length }}
+                    </p>
+                    <template v-for="application in selectedApplications" :key="application.id">
+                        <active-headquarters-selected :application="application" :action="action" @select="onToggleSelect" />
+                    </template>
+
+                    <Button class="save" type="button" label="Сохранить" @click="onAction"></Button>
+                </template>
+            </div>
+        </template>
+    </div>
+</template>
+
+<script setup>
+import { ref, watch } from 'vue';
+import { Button } from '@shared/components/buttons';
+
+import ActiveHeadquartersItem from './ActiveHeadquartersItem.vue';
+import ActiveHeadquartersSelected from './ActiveHeadquartersSelected.vue';
+
+const loading = ref(false);
+const action = ref('Одобрить');
+const actionsList = ref(['Одобрить', 'Отклонить']);
+const isCheckedAll = ref(false);
+
+const selectedApplications = ref([]);
+
+const applicationsData = ref([
+    {
+        id:1,
+        user: {
+            id: 1,
+            name: 'Васильев Андрей Владимирович1',
+            date_of_birth: '13.07.2000',
+        },
+        headquarters: {
+            id: 1,
+            name: 'Штаб Кировского РО',
+            photo: '',
+        },
+        selected: false,
+    },
+    {
+        id:2,
+        user: {
+            id: 2,
+            name: 'Васильев Андрей Владимирович2',
+            date_of_birth: '13.07.2000',
+            media:{
+                avatar: '123',
+            }
+        },
+        headquarters: {
+            id: 1,
+            name: 'Штаб Кировского РО',
+            photo: '',
+        },
+        selected: false,
+    },
+    {
+        id:3,
+        user: {
+            id: 3,
+            name: 'Васильев Андрей Владимирович3',
+            date_of_birth: '13.07.2000',
+            media:{
+                avatar: '',
+            }
+        },
+        headquarters: {
+            id: 1,
+            name: 'Штаб Кировского РО',
+            photo: '',
+        },
+        selected: false,
+    },
+])
+
+const onChooseAll = () => {
+    console.log(isCheckedAll.value);
+    if (isCheckedAll.value){
+        for(let application of applicationsData.value){
+            application.selected = true;
+        }
+        selectedApplications.value = applicationsData.value;
+    } else {
+        selectedApplications.value = [];
+        for(let application of applicationsData.value){
+            application.selected = false;
+        }
+    }
+}
+
+const onToggleSelect = (application, isChecked) => {
+    if (isChecked) {
+        application.selected = isChecked;
+        selectedApplications.value = [...selectedApplications.value, application];
+    } else {
+        application.selected = isChecked;
+        selectedApplications.value = selectedApplications.value.filter(
+            (a) => a.id !== application.id,
+        );
+    }
+};
+
+const onAction = () => {
+    console.log('send');
+}
+
+watch(selectedApplications, (selectedApplications) => {
+    isCheckedAll.value =
+        selectedApplications.length == selectedApplications.value;
+});
+</script>
+
+<style scoped lang="scss">
+.competitions__actions {
+    display: grid;
+    width: 100%;
+    margin-bottom: 40px;
+    grid-template-columns: 48px 224px;
+    justify-content: space-between;
+    // width: 224px;
+    height: 48px;
+    gap: 10px;
+
+    font-family: Bert Sans;
+    font-size: 16px;
+    font-weight: 400;
+    line-height: 21px;
+    letter-spacing: 0em;
+    text-align: left;
+}
+
+.competitions__actions-select {
+    background-color: inherit;
+    min-width: 224px;
+    border-radius: 10px;
+    border: 1px solid #b6b6b6;
+    font-family: Bert Sans;
+    font-size: 16px;
+    font-weight: 400;
+    line-height: 21px;
+    letter-spacing: 0em;
+    text-align: left;
+}
+
+:deep(.v-field) {
+    border-radius: 10px;
+}
+
+.competitions__btns {
+    display: grid;
+    width: 100%;
+    justify-content: center;
+    margin-top: 60px;
+    font-family: Bert Sans;
+    font-size: 16px;
+    font-weight: 400;
+    line-height: 21px;
+    letter-spacing: 0em;
+    text-align: left;
+}
+
+.text_total {
+    width: 1180px;
+    height: 26px;
+    margin: 40px 0;
+    font-family: Bert Sans;
+    font-size: 20px;
+    font-weight: 600;
+    line-height: 26px;
+    letter-spacing: 0em;
+    text-align: left;
+    color: #35383f;
+}
+
+:deep(.v-select__selection-text) {
+    font-family: Bert Sans;
+    font-size: 16px;
+    font-weight: 400;
+    line-height: 21px;
+    letter-spacing: 0em;
+    text-align: left;
+}
+
+.competition__item {
+    display: grid;
+    width: 100%;
+    gap: 12px;
+}
+
+.competition__content {
+    display: grid;
+    width: 100%;
+    grid-gap: 12px;
+    grid-template-columns: 48px minmax(200px, 300px) minmax(200px, 300px) 1fr 100px;
+
+    font-family: Bert Sans;
+    font-size: 16px;
+    font-weight: 500;
+    line-height: 21px;
+    letter-spacing: 0em;
+    text-align: left;
+}
+
+.horizontal__confidant {
+    padding: 10px 10px;
+    border: 1px solid #b6b6b6;
+    border-radius: 10px;
+
+    input {
+        width: 100%;
+        height: 100%;
+    }
+}
+.competitions__container{
+    margin-bottom: 80px;
+}
+</style>
