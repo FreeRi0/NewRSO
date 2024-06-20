ec74d9d3c7a1d2b665f45d9bd834b0f0771bdffa
Sergio Alferov <alferovsergio@yandex.ru>
2024-06-20 18:11:03 +0300

    fix

--- Client/src/pages/ContributionPage/components/ContributorPay.vue
+++ Client/src/pages/ContributionPage/components/ContributorPay.vue
@@ -319,6 +319,7 @@ const updateReg = (regVal) => {
     } else if (levelAccess.value < 2) {
         search = '?district_headquarter__name=' + district.value;
     }
+
     if (name.value) search += '&search=' + name.value;
     viewContributorsData(search);
     getFiltersData('/locals/', search);
@@ -382,20 +383,20 @@ const updateMembership = (membershipVal) => {
         search.push('membership_fee=false');
     }
     if (district.value) {
-            search.push('district_headquarter__name=' + district.value);
-        }
-        if (reg.value) {
-            search.push('regional_headquarter__name=' + reg.value);
-        }
-        if (local.value) {
-            search.push('local_headquarter__name=' + local.value);
-        }
-        if (educ.value) {
-            search.push('educational_headquarter__name=' + educ.value);
-        }
-        if (detachment.value) {
-            search.push('detachment__name=' + detachment.value);
-        }
+        search.push('district_headquarter__name=' + district.value);
+    }
+    if (reg.value) {
+        search.push('regional_headquarter__name=' + reg.value);
+    }
+    if (local.value) {
+        search.push('local_headquarter__name=' + local.value);
+    }
+    if (educ.value) {
+        search.push('educational_headquarter__name=' + educ.value);
+    }
+    if (detachment.value) {
+        search.push('detachment__name=' + detachment.value);
+    }
     if (name.value) search.push('&search=' + name.value);
     viewContributorsData('?' + search.join('&'));
 
@@ -692,23 +693,25 @@ watch(
 watch(
     () => membership.value,
     () => {
-        let search = '';
+        let search = [];
         if (district.value) {
-            search += '?district_headquarter__name=' + district.value;
+            search.push('district_headquarter__name=' + district.value);
         }
         if (reg.value) {
-            search += '?regional_headquarter__name=' + reg.value;
+            search.push('regional_headquarter__name=' + reg.value);
         }
         if (local.value) {
-            search += '?local_headquarter__name=' + local.value;
+            search.push('local_headquarter__name=' + local.value);
         }
         if (educ.value) {
-            search += '?educational_headquarter__name=' + educ.value;
+            search.push('educational_headquarter__name=' + educ.value);
         }
         if (detachment.value) {
-            search = '?detachment__name=' + detachment.value;
+            search.push('detachment__name=' + detachment.value);
         }
-        viewContributorsData(search, '', participants.value.length);
+        if (name.value) search.push('&search=' + name.value);
+        viewContributorsData('?' + search.join('&'));
+        // viewContributorsData(search, '', participants.value.length);
     },
 );
 
