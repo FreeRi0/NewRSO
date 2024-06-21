27b9cd991f3376a2e56930e55a20137463c78e49
Styxk1 <styxk96@gmail.com>
2024-06-21 13:30:19 +0300

    Переименовал табы на множественное число

--- Client/src/pages/ActiveApplicationsData/components/ActiveApplicationsData.vue
+++ Client/src/pages/ActiveApplicationsData/components/ActiveApplicationsData.vue
@@ -5,7 +5,7 @@
 
             <div class="d-flex mt-9 mb-9 active-tabs">
                 <button class="contributorBtn" :class="{ active: picked === tab.name }" v-for="tab in tabs"
-                    v-show="tab.name === 'Верификация аккаунтов' && (roleStore.roles.detachment_commander || roleStore.roles.regionalheadquarter_commander) || tab.name === 'Заявка на вступление в отряд' && (roleStore.roles.detachment_commander) || tab.name === 'Заявка на участие в мероприятии' || tab.name === 'Конкурсы' || tab.name === 'Заявка на вступление в штаб'"
+                    v-show="tab.name === 'Верификация аккаунтов' && (roleStore.roles.detachment_commander || roleStore.roles.regionalheadquarter_commander) || tab.name === 'Заявки на вступление в отряд' && (roleStore.roles.detachment_commander) || tab.name === 'Заявки на участие в мероприятии' || tab.name === 'Конкурсы' || tab.name === 'Заявки на вступление в штаб'"
                     :key="tab.id" @click="picked = tab.name">
                     {{ tab.name }}
                 </button>
@@ -57,19 +57,19 @@ const tabs = ref([
     },
     {
         id: '2',
-        name: 'Заявка на вступление в отряд',
+        name: 'Заявки на вступление в отряд',
 
 
     },
     {
         id: '3',
-        name: 'Заявка на вступление в штаб',
+        name: 'Заявки на вступление в штаб',
 
 
     },
     {
         id: '4',
-        name: 'Заявка на участие в мероприятии',
+        name: 'Заявки на участие в мероприятии',
 
 
     },
