cf637e238625d73589f4d1fa01bfecd37c45fe8b
Sergio Alferov <alferovsergio@yandex.ru>
2024-06-26 18:13:12 +0300

    fix

--- Client/src/features/Authorization/AuthorizationWithEmail/components/AuthorizationWithEmail.vue
+++ Client/src/features/Authorization/AuthorizationWithEmail/components/AuthorizationWithEmail.vue
@@ -61,7 +61,7 @@ const swal = inject('$swal');
 
 
 const APP_ID = 51915086
-const REDIRECT_URL = 'https://xn--j1ab.xn--d1amqcgedd.xn--p1ai'
+const REDIRECT_URL = 'https://xn--j1ab.xn--d1amqcgedd.xn--p1ai/my-page/'
 
 const oneTap = new VKID.OneTap();
 
