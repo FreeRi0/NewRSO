57942eaf4ba0130a7268184321bb1673edc07e99
Sergio Alferov <alferovsergio@yandex.ru>
2024-06-27 15:16:05 +0300

    vk new auth

---
+++ Client/src/shared/components/dropdown/addModal.vue
@@ -0,0 +1,110 @@
+<template>
+  <div class="modal">
+    <div class="modal__wrapper">
+      <p class="modal__wrapper_title">При одобрении вашей заявки на вступление в штаб, вы будете автоматически удалены
+        из тех ЛСО/штабов, в которых вы сейчас состоите.</p>
+
+      <div class="modal__wrapper_buttons">
+        <Button type="button" label="Продолжить" class="modal__wrapper_buttons_confirm" @click="add"></Button>
+        <Button type="button" label="Отменить" class="modal__wrapper_buttons_cancel" @click="close"></Button>
+      </div>
+    </div>
+  </div>
+</template>
+<script setup>
+import { Button } from '@shared/components/buttons';
+
+const emit = defineEmits(['close', 'add']);
+
+function close() {
+  emit('close');
+}
+
+function add() {
+  emit('add');
+}
+</script>
+<style lang="scss" scoped>
+.modal {
+  border: 2px solid #35383F;
+  max-width: 400px;
+  height: 270px;
+  border-radius: 10px;
+  position: absolute;
+  right: 35%;
+  background: #fff;
+  z-index: 999;
+  transform: none;
+
+  @media (max-width: 575px) {
+    max-width: 320px;
+    height: 216px;
+    right: auto;
+  }
+
+  &__wrapper {
+    padding: 40px 32px;
+
+    @media (max-width: 575px) {
+      padding: 25px 20px;
+    }
+
+    &_title {
+      font-size: 16px;
+      font-weight: 600;
+      font-family: Bert Sans;
+      text-align: center;
+      max-width: 336px;
+      line-height: 21.1px;
+
+      @media (max-width: 575px) {
+        font-size: 14px;
+      }
+    }
+
+    &_buttons {
+      margin-top: 32px;
+      display: flex;
+      flex-direction: column;
+      row-gap: 12px;
+
+      @media (max-width: 575px) {
+        margin-top: 28px;
+        row-gap: 8px;
+      }
+
+      &_confirm {
+        margin: 0px;
+        font-size: 16px;
+        font-weight: 600;
+        font-family: Bert Sans;
+        height: 52px;
+
+        @media (max-width: 575px) {
+          height: 40px;
+          font-size: 14px;
+          padding: 5px 32px;
+        }
+      }
+
+      &_cancel {
+        margin: 0px;
+        border: 2px solid #35383F;
+        background-color: #fff;
+        color: #35383F;
+        font-size: 16px;
+        font-weight: 600;
+        font-family: Bert Sans;
+        height: 52px;
+
+        @media (max-width: 575px) {
+          height: 40px;
+          font-size: 14px;
+
+          padding: 5px 32px;
+        }
+      }
+    }
+  }
+}
+</style>
