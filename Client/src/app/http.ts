b67620df2bfd72f55158d6a37349d3c03953bad8
Sergio Alferov <alferovsergio@yandex.ru>
2024-07-13 17:14:49 +0300

    fix

--- Client/src/app/http.ts
+++ Client/src/app/http.ts
@@ -1,31 +1,31 @@
 import axios from 'axios';
-
-
+import router from "./router";
 export const HTTP = axios.create({
-    //baseURL: 'https://xn--j1ab.xn--d1amqcgedd.xn--p1ai/api/v1/',
-    baseURL: 'http://213.139.208.147:30000/api/v1/',
-    //baseURL: 'https://rso.sprint.1t.ru/api/v1/',
-    headers: {
-        Accept: 'application/json',
-        'Content-Type': 'application/json',
-    },
+  //baseURL: 'https://xn--j1ab.xn--d1amqcgedd.xn--p1ai/api/v1/',
+  baseURL: 'http://213.139.208.147:30000/api/v1/',
+  //baseURL: 'https://rso.sprint.1t.ru/api/v1/',
+  headers: {
+    Accept: 'application/json',
+    'Content-Type': 'application/json',
+  },
 });
+
+
 
 HTTP.interceptors.request.use(
   (config) => {
-
-        if (config.url == '/jwt/create/' || config.url == '/register/' || config.url == '/exchange-token/' || config.url == '/jwt/vk-login/' || config.url == '/jwt/verify/' || config.url == '/jwt/refresh/' || config.url == '/regions/' || config.url =='/reset_password/') {
-            delete config.headers.Authorization;
-        } else {
-            config.headers.Authorization =
-                'JWT ' + localStorage.getItem('jwt_token');
-        }
-        return config;
-    },
-    function (error) {
-        console.log('aborted');
-        return error;
-    },
+    if (config.url == '/jwt/create/' || config.url == '/register/' || config.url == '/exchange-token/' || config.url == '/jwt/vk-login/' || config.url == '/jwt/verify/' || config.url == '/jwt/refresh/' || config.url == '/regions/' || config.url == '/reset_password/') {
+      delete config.headers.Authorization;
+    } else {
+      config.headers.Authorization =
+        'JWT ' + localStorage.getItem('jwt_token');
+    }
+    return config;
+  },
+  function (error) {
+    console.log('aborted');
+    return error;
+  },
 );
 HTTP.interceptors.response.use(
   (res) => {
@@ -42,7 +42,8 @@ HTTP.interceptors.response.use(
           }
           else {
             console.log('401');
-            window.location.href = 'http://localhost:8080/'
+
+            router.push({ name: 'Login' });
           }
         } catch (error) {
           return Promise.reject(error);
@@ -50,7 +51,9 @@ HTTP.interceptors.response.use(
       }
 
       if (err.response.status === 403 && err.response.data) {
-        window.location.href = 'http://localhost:8080/my-page'
+
+        console.log('403 err', router);
+        router.push({ name: 'mypage' });
         return Promise.reject(err.response.data);
       }
     }
