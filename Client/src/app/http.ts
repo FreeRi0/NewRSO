42b5a82fe5060225449fba37c4a9fc748fb0220c
Yulia <lyoulka@mail.ru>
2024-07-18 09:21:35 +0400

    http fix

--- Client/src/app/http.ts
+++ Client/src/app/http.ts
@@ -1,6 +1,8 @@
 import axios from 'axios';
 import { useUserStore } from '@features/store';
 import router from "./router";
+import { ref } from 'vue';
+
 
 
 export const HTTP = axios.create({
@@ -13,6 +15,7 @@ export const HTTP = axios.create({
   },
 });
 
+let refreshTokenPromise = ref({})
 
 HTTP.interceptors.request.use(
   (config) => {
@@ -38,23 +41,66 @@ HTTP.interceptors.response.use(
     if (err.response) {
       // Access Token was expired
       if (err.response.status === 401) {
+        const originalRequest = err.config;
+        const userStore = useUserStore();
         try {
-          if (localStorage.getItem('jwt_token') !== null) {
-            await updateToken();
+          console.log('here')
+          console.log(originalRequest.url)
+          if (localStorage.getItem('jwt_token') && originalRequest.url !== '/jwt/refresh/') {
+            console.log('here 1')
+            console.log('refreshTokenPromise', JSON.stringify(refreshTokenPromise) === JSON.stringify({}), JSON.stringify(refreshTokenPromise.value) === JSON.stringify({}))
+            if (JSON.stringify(refreshTokenPromise.value) === JSON.stringify({})) {
+              console.log('here 2')
+              refreshTokenPromise.value = HTTP.post('/jwt/refresh/', {
+                refresh: localStorage.getItem('refresh_token'),
+              })
+                .then((response) => {
+
+                  console.log('here 3')
+
+                  localStorage.setItem('jwt_token', response.data.access);
+                  localStorage.setItem('refresh_token', response.data.refresh);
+                  refreshTokenPromise.value = {};
+                  originalRequest._retry = true;
+                  return HTTP(originalRequest);
+                })
+                .catch(() => {
+                  console.log('here 4')
+                  refreshTokenPromise.value = {};
+                  userStore.logOut();
+                  localStorage.removeItem('jwt_token');
+                  router.push({ name: 'Login' });
+                })
+            } else {
+              console.log('here 5')
+              console.log('jwt_token', localStorage.getItem('jwt_token'))
+              setTimeout(() => {
+                if (localStorage.getItem('jwt_token') !== null) {
+                  console.log('here 6')
+                  originalRequest._retry = true;
+                  return HTTP(originalRequest);
+                }
+              }, 2000);
+            }
+            // await updateToken();
           } else {
+            console.log('here 7')
+            userStore.logOut();
+            localStorage.removeItem('jwt_token');
             router.push({ name: 'Login' });
           }
         } catch (error) {
+          console.log('here 8')
           return Promise.reject(error);
         }
       }
 
-      /*if (err.response.status === 403 && err.response.data) {
+      if (err.response.status === 403 && err.response.data) {
 
         console.log('403 err', router);
-        router.push({ name: 'mypage' });
+        // router.push({ name: 'mypage' });
         return Promise.reject(err.response.data);
-      }*/
+      }
     }
 
     return Promise.reject(err);
@@ -62,7 +108,7 @@ HTTP.interceptors.response.use(
 );
 
 
-const updateToken = async () => {
+/*const updateToken = async () => {
   try {
     const resp = await HTTP.post('/jwt/refresh/', {
       refresh: localStorage.getItem('refresh_token'),
@@ -83,5 +129,5 @@ const updateToken = async () => {
     localStorage.removeItem('jwt_token');
     router.push({ name: 'Login' });
   }
-};
+};*/
 
