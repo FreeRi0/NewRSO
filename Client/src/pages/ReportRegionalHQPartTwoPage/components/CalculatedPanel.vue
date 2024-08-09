ab12db7304dea60c508400576f5fe7daba2c6b2d
Zondkun <gfneer1990@gmail.com>
2024-08-09 19:16:48 +0500

    CalculatedPannel added

---
+++ Client/src/pages/ReportRegionalHQPartTwoPage/components/CalculatedPanel.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="form__field-group">
+        <div class="form__field-group-text">
+            <img src="@app/assets/icon/alert.svg" alt="alert" class="alert">
+            <h2 class="report_title-h3">{{ props.text }}</h2>
+        </div>
+    </div>
+</template>
+
+<script setup>
+const props = defineProps({
+    text: {
+        type: String,
+    },
+})
+</script>
