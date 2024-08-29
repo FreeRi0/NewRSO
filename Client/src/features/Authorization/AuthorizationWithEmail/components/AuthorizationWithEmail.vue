5c996a461832345e69453eb16da1d241feece067
Sergio Alferov <alferovsergio@yandex.ru>
2024-08-29 11:42:33 +0300

    fix

--- Client/src/features/Authorization/AuthorizationWithEmail/components/AuthorizationWithEmail.vue
+++ Client/src/features/Authorization/AuthorizationWithEmail/components/AuthorizationWithEmail.vue
@@ -104,6 +104,7 @@ const LoginUser = async () => {
         userStore.getUser();
         userStore.getCountApp();
         roleStore.getRoles();
+        roleStore.getMyPositions();
         roleStore.getUserParticipantsStatus(competition_pk);
 
 
