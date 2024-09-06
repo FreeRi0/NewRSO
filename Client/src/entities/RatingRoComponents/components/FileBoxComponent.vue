d2745269ad6f46b5f61689d681d6609d52586ab8
kozyrek <kozyrevaia@bk.ru>
2024-09-06 17:58:41 +0500

    добавляет адаптив показатель 18

---
+++ Client/src/entities/RatingRoComponents/components/FileBoxComponent.vue
@@ -0,0 +1,51 @@
+<template>
+    <div class="report__file-box">
+        <span class="report__file-name">
+            <SvgIcon v-if="fileType === 'jpg'" icon-name="file-jpg" />
+            <SvgIcon v-if="fileType === 'pdf'" icon-name="file-pdf" />
+            <SvgIcon v-if="fileType === 'png'" icon-name="file-png" />
+            {{ file }}
+        </span>
+
+        <span class="report__file-size">
+            {{ fileSize }} Мб
+        </span>
+
+        <button 
+            @click="clickOnButton"
+            class="report__button-delete-file"
+            v-bind="$attrs"
+        >
+            Удалить
+        </button>
+    </div>
+</template>
+
+<script setup>
+import { SvgIcon } from '@shared/index';
+
+defineOptions({
+    inheritAttrs: false,
+});
+
+const props = defineProps({
+    file: {
+        type: String,
+        default: null,
+    },
+    fileType: {
+        type: String,
+        default: null,
+    },
+    fileSize: {
+        type: Number,
+        default: null,
+    },
+});
+
+const emit = defineEmits(['click']);
+
+const clickOnButton = () => {
+    emit('click');
+};
+</script>
--- Client/src/features/Authorization/AuthorizationWithEmail/components/AuthorizationWithEmail.vue
+++ Client/src/features/Authorization/AuthorizationWithEmail/components/AuthorizationWithEmail.vue
@@ -36,7 +36,7 @@
     </div>
 </template>
 
-<script setup>
+<script setup scoped>
 import { ref, inject, onMounted } from 'vue';
 import { Button } from '@shared/components/buttons';
 import { Input, passwordInput } from '@shared/components/inputs';
