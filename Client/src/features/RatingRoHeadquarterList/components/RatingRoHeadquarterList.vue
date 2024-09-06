a92d96811037f25bcb6732e83928b50a79313448
Sergio Alferov <alferovsergio@yandex.ru>
2024-09-06 09:59:14 +0300

    fix

--- Client/src/features/RatingRoHeadquarterList/components/RatingRoHeadquarterList.vue
+++ Client/src/features/RatingRoHeadquarterList/components/RatingRoHeadquarterList.vue
@@ -12,7 +12,7 @@ const props = defineProps({
 })
 const goToReport = (id) => {
   router.push({
-    name: 'ReportRegionalPartOne',
+    name: 'ReportRegionalPartOneComander',
     query: {
       id: id,
     },
