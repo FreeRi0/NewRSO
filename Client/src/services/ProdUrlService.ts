edd8985f6e308673b43d3f45491a1071e4ef3667
Sergio Alferov <alferovsergio@yandex.ru>
2024-09-30 19:10:16 +0300

    fix show

--- Client/src/services/ProdUrlService.ts
+++ Client/src/services/ProdUrlService.ts
@@ -1,6 +1,6 @@
 export function showByUrl() {
   let value = false;
-  if (window.location.hostname === 'localhost' || window.location.hostname === 'rso.sprint.1t' || window.location.hostname === '213.129.208.147') {
+  if (window.location.hostname === 'localhost' || window.location.hostname === 'rso.sprint.1t' || window.location.hostname === '213.129.208.147:30000') {
     value = true;
   }
   return value;
