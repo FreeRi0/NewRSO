0eab50baca101ece1b3a5976bed48f777ae0836d
Sergio Alferov <alferovsergio@yandex.ru>
2024-11-13 20:00:15 +0300

    fix

--- Client/src/pages/ReportRegionalHQPartTwoPage/components/SeventhPanelForm.vue
+++ Client/src/pages/ReportRegionalHQPartTwoPage/components/SeventhPanelForm.vue
@@ -1142,7 +1142,7 @@ const returnForReviewNinth = (event) => {
                 }
             }
 
-            if (reportStore.reportForCheckCH.six[props.sixId]?.rejecting_reasons !== undefined) {
+            if (reportStore.reportForCheckCH.six[props.sixId]?.rejecting_reasons !== null) {
                 reportStore.returnReport.six[props.sixId] = true;
             }
             emit('getId', props.id);
@@ -1249,7 +1249,7 @@ const returnForReviewNinth = (event) => {
                         fileCH.value.type = reportStore.reportDataCHFile.ninth[props.ninthId].type.split('/').at(-1);
                         fileCH.value.size = reportStore.reportDataCHFile.ninth[props.ninthId].size / Math.pow(1024, 2);
                     }
-                    if (reportStore.reportForCheckCH.ninth[props.ninthId]?.rejecting_reasons !== undefined) {
+                    if (reportStore.reportForCheckCH.ninth[props.ninthId]?.rejecting_reasons !== null) {
                         reportStore.returnReport.ninth[props.ninthId] = true;
                     }
                 }
